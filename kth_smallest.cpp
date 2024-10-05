82307a26d72380f85f111db8fe54def634df06da
Dhruv Jain <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-10-05 10:01:37 +0000

    added kth smallest element code

---
+++ kth_smallest.cpp
@@ -0,0 +1,75 @@
+/*Given an array arr[] and an integer k where k is smaller than the size of the array, the task is to find the kth smallest element in the given array.
+
+Follow up: Don't solve it using the inbuilt sort function.
+
+Examples :
+
+Input: arr[] = [7, 10, 4, 3, 20, 15], k = 3
+Output:  7
+Explanation: 3rd smallest element in the given array is 7.
+Input: arr[] = [2, 3, 1, 20, 15], k = 4 
+Output: 15
+Explanation: 4th smallest element in the given array is 15.*/
+
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
+        priority_queue<int>pq; //max_heap
+        for(int i = 0; i < k; i++){
+            pq.push(arr[i]);
+        }
+        for(int i = k; i<arr.size(); i++){
+            if(pq.top() > arr[i]){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+        return pq.top();
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int k;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        k = crr[0];
+        int n = arr.size();
+        Solution ob;
+        cout << ob.kthSmallest(arr, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
