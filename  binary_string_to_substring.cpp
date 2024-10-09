6beb3b45240f6e6807a6b040418e2c036a4e039e
Dhruv Jain <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-10-09 14:33:30 +0000

    added binary string to substring

---
+++  binary_string_to_substring.cpp
@@ -0,0 +1,11 @@
+/*Split the binary string into substrings with equal number of 0s and 1s
+Difficulty: EasyAccuracy: 50.02%Submissions: 25K+Points: 2
+Given binary string str of length N. The task is to find the maximum count of consecutive substrings str can be divided into such that all the substrings are balanced i.e. they have an equal number of 0s and 1s. If it is not possible to split str satisfying the conditions then return -1.
+
+Example 1:
+
+Input:
+S = "0100110101"
+Output: 4
+Explanation: 
+The required substrings are 01, 0011, 01 and 01*/.
